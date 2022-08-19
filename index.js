2bb355ce0790c1091037f37ad94546bfccb8bd93
+ <awino.millicent@student.moringaschool.com>
2022-08-20 00:36:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+];
+function destructivelyAppendCat(Ralph){
+    return cats.push (Ralph);
+}
+function destructivelyPrependCat(Bob){
+    return cats.unshift (Bob);
+}
+function destructivelyRemoveLastCat(Bob){
+    return cats.pop (Bob);
+}
+function destructivelyRemoveFirstCat(Milo){
+   return cats.shift(Milo);
+}
+function appendCat(Broom){
+    return [...cats, Broom];
+}
+function prependCat(Arnold){
+return [Arnold, ...cats];
+}
+function removeLastCat(){
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
